c1a3948a5b46d09eaeed6c3b5c37d887be567699
christian <cristianyamith@hotmail.com>
2024-09-15 22:20:03 -0500

    feat, agrego el header de la pagina

--- proyectoPlatzi/src/app/app.component.ts
+++ proyectoPlatzi/src/app/app.component.ts
@@ -10,4 +10,22 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'proyectoPlatzi';
+  titulo ='MI PAGINA';
+ 
+  navegacion={
+    opcion1:'HOME',
+    opcion2:'NOSOTROS',
+    opcion3:'AYUDA',
+    opcion4:'CONTACTOS',
+    iconHome:'home',
+    iconNosotros:'info',
+    iconAyuda:'file_open',
+    iconContactos:'person',
+
+  };
+  imglogo ='https://upload.wikimedia.org/wikipedia/commons/3/34/YouTube_logo_%282017%29.png';
+  fotoPerfil = 'https://cdn-images.livecareer.es/pages/foto_cv_lc_es_7.jpg';
+ 
+
 }
+
